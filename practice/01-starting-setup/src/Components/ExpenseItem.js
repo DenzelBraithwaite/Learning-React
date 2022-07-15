60b5953b8fab788059358c47fd2a46873833e23e
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-14 20:11:57 -0400

    adding css using styled components

--- practice/01-starting-setup/src/Components/ExpenseItem.js
+++ practice/01-starting-setup/src/Components/ExpenseItem.js
@@ -2,8 +2,14 @@ import './CSS/ExpenseItem.css'
 
 const ExpenseItem = () => {
   return (
-
-  )
+    <div className="expense-item">
+      <div >July 14th 2022</div>
+      <div className="expense-item__description">
+        <h2>New Tv</h2>
+        <div className="expense-item__price">$500</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
