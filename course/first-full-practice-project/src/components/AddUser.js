a55141b6d950de8b87d7a19bf06ea44c8130f1d6
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-16 16:44:36 -0400

    refactored modal logic, now using an object for title and message

--- course/first-full-practice-project/src/components/AddUser.js
+++ course/first-full-practice-project/src/components/AddUser.js
@@ -9,18 +9,25 @@ const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
     const [isValid, setIsValid] = useState(true);
-    const [modalTitle, setModalTitle] = useState("Something went wrong.");
-    const [modalMessage, setModalMessage] = useState('Please try again later');
+    // const [modalTitle, setModalTitle] = useState("Something went wrong.");
+    // const [modalMessage, setModalMessage] = useState('Please try again later');
+    const [modalInfo, setModalInfo] = useState({title: 'Something went wrong', message: 'please try again'})
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-            ModalHandler("Username too short", "Username cannot be left blank");
+            ModalHandler({
+                title: "Input fields cannot be left blank.",
+                message: "Please enter a valid username and age."
+            });
             return;
         }
 
-        if (+enteredAge <= 0) {
-            ModalHandler("Age too low", "Age less than 1");
+        if (+enteredAge < 18) {
+            ModalHandler({
+                title: "You must be 18 or over to add a user.",
+                message: "Please enter a valid age."
+            });
             return;
         }
         
@@ -37,19 +44,18 @@ const AddUser = props => {
         setEnteredAge(event.target.value);
     };
 
-    const ModalHandler = (title, message) => {
-        setModalTitle(title)
-        setModalMessage(message)
+    const ModalHandler = (modalInfo) => {
+        setModalInfo({title: modalInfo.title, message: modalInfo.message})
         setIsValid(false);
     };
 
-    const closeModalHandler = (title, message) => {
+    const closeModalHandler = () => {
         setIsValid(true);
     };
 
     return(
         <div>
-           {!isValid && <Modal onCloseModal={closeModalHandler} title={modalTitle} message={modalMessage} />}
+           {!isValid && <Modal onCloseModal={closeModalHandler} title={modalInfo.title} message={modalInfo.message} />}
             <Card className={cssClasses.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
