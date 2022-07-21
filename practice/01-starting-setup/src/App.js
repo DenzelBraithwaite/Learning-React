263b6da364061b21542dfe83e652b36660a582c6
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-21 10:27:52 -0400

    practiced project

--- practice/01-starting-setup/src/App.js
+++ practice/01-starting-setup/src/App.js
@@ -1,23 +1,23 @@
-import Expenses from './components/expenses/Expenses'
+import Expenses from "./components/expenses/Expenses";
 
 const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
@@ -26,9 +26,9 @@ const App = () => {
   return (
     <div>
       <h2>Expense watcher!</h2>
-      <Expenses expenseItems={expenses}/>
+      <Expenses expenseItems={expenses} />
     </div>
   );
-}
+};
 
 export default App;
