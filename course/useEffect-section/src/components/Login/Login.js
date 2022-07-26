93f741a24bf09d97549abe944a158653b62f20ec
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-07-26 11:41:59 -0400

    finished with useContext, next adding input component

--- course/useEffect-section/src/components/Login/Login.js
+++ course/useEffect-section/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/authContext';
 
 const emailReducer = (state, action) => {
     if (action.type === 'USER_INPUT') {
@@ -33,13 +34,15 @@ const Login = props => {
 
     const [emailState, dispatchEmail] = useReducer(emailReducer, {
         value: '',
-        isValid: null,
+        isValid: null
     });
 
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
         value: '',
-        isValid: null,
+        isValid: null
     });
+
+    const context = useContext(AuthContext);
 
     useEffect(() => {
         console.log('Effect RUNNING');
@@ -86,7 +89,7 @@ const Login = props => {
 
     const submitHandler = event => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        context.onLogin(emailState.value, passwordState.value);
     };
 
     return (
